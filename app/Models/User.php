3daf734450687fbf3cb69eab28bb345778c7ce90
Afowowe Olumide Victor <volumide42@gmail.com>
2023-05-06 08:51:10 +0100

    add user controller

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'phone',
+        'state_of_residence',
+        'country',
         'email',
         'password',
     ];
